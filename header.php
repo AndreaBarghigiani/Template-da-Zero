35a9871c8c330300cc6930167fbd9608239a770f
Pr0v4 <a.barghigiani@gmail.com>
2012-10-10 00:28:51 +0200

    Sviluppiamo la Testata del nostro Tema
    Tutte le modifiche applicate fino al termine della lezione video

---
+++ header.php
@@ -0,0 +1,27 @@
+<!-- Inizio Header -->
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html lang="en"> <![endif]-->
+<!--[if IE 7]>    <html lang="en"> <![endif]-->
+<!--[if IE 8]>    <html lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	 
+	<title><?php wp_title(''); ?></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url'); ?>">
+	<script src="<?php bloginfo( 'template_url' ); ?>/js/libs/modernizr.min.js"></script>
+</head>
+ 
+<body>
+ 
+	<div id="container">
+	 
+		<header>
+		 
+		</header>
+		<!-- Fine Header -->
