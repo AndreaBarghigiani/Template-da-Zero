db74a6f881a1d3eec54116d1bd6199353ed5f9fc
Pr0v4 <a.barghigiani@gmail.com>
2012-10-09 23:06:37 +0200

    test

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Questo file conterra' alcune regole principali per il nostro tema
+ * ma per mantenere la struttura dei file WordPress il piu'
+ * organizzata possibile saltuariamente caricheremo file dalla
+ * cartella inc/ 
+ */
+ 
+ //Carico il file esterno per le impostazioni del tema
+ require_once( TEMPLATEPATH . 'inc/impostazioni_tema.php');
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <!-- Inizio Content -->
 <div id="main" role="main">
-	
+	<p>test</p>
 	<?php get_sidebar(); ?>
 	
 </div>
