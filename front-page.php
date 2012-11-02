8c28a250720644524592179b19d8236dcf23939d
Pr0v4 <a.barghigiani@gmail.com>
2012-11-02 16:45:17 +0100

    Lezione 15
    Scopriamo come sia possibile andare a creare una FrontPage
    personalizzata per il nostro template WordPress.

---
+++ front-page.php
@@ -0,0 +1,54 @@
+<?php get_header(); ?>
+ 
+<!-- Inizio Content -->
+    <div id="main" role="main">
+ 
+ 		<?php
+		    $loop = new WP_Query( 'category_name=parent-category-i&posts_per_page=1' );
+		    while( $loop->have_posts() ) : $loop->the_post();
+		?>
+			<h3>Categoria Parent Category I</h3>
+		    <div class="parent-i">
+		        <?php the_title(); ?>
+		    </div>
+		<?php
+		    endwhile;
+		    wp_reset_query();
+		?>
+		 
+		<!-- Mostro Parent Category II -->
+		<h3>Categoria Parent Category II</h3>
+		<?php
+		    $loop = new WP_Query( 'category_name=parent-category-ii&posts_per_page=2' );
+		    while( $loop->have_posts() ) : $loop->the_post();
+		?>
+			
+		    <div class="parent-ii">
+		        <?php the_title(); ?>
+		    </div>
+		<?php
+		    endwhile;
+		    wp_reset_query();
+		?>
+		 
+		<!-- Mostro Parent Category III -->
+		<?php
+		    $loop = new WP_Query( 'category_name=parent-category-iii&posts_per_page=1' );
+		    while( $loop->have_posts() ) : $loop->the_post();
+		?>
+			<h3>Categoria Parent Category III</h3>
+		    <div class="parent-iii">
+		        <?php the_title(); ?>
+		    </div>
+		<?php
+		    endwhile;
+		    wp_reset_query();
+		?>
+ 
+ 
+    </div>
+<!-- Fine Content -->
+ 
+<?php get_sidebar(); ?>
+ 
+<?php get_footer(); ?>
